41dddae3199cb8b56955ac59f7aaffbf99d1b1fc
nathanbc32 <2030699@apps.nsd.org>
2025-07-19 14:56:49 -0700

    Add Sensitive.h for application keys and WiFi credentials

---
+++ src/Sensitive.h
@@ -0,0 +1,20 @@
+#define APP_KEY "xxxxxxxxxxxxxx"     // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
+#define APP_SECRET "xxxxxxxxxxxxxx"  // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
+#define TEMP_ID "xxxxxxxxxxxxxx"     // Should look like "5dc1564130xxxxxxxxxxxxxx"
+#define SWITCH_ID "xxxxxxxxxxxxxx"
+#define LIGHT_ID "xxxxxxxxxxxxxx"
+
+// Fill with own WiFi credentials
+const String SSID[] = {
+    "xxxxxxxxxxxxxx",
+    "xxxxxxxxxxxxxx",
+    "xxxxxxxxxxxxxx",
+    "xxxxxxxxxxxxxx"
+};
+
+const String PASS[] = {
+    "xxxxxxxxxxxxxx",
+    "xxxxxxxxxxxxxx",
+    "xxxxxxxxxxxxxx",
+    "xxxxxxxxxxxxxx"
+};
