e48c19239013ecb57de08db4f75ef4d135b8c395
nathanbc32 <2030699@apps.nsd.org>
2025-07-19 16:04:51 -0700

    change sensitive.h to sensitive.example.h

--- src/Sensitive.example.h
+++ src/Sensitive.example.h
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+
 #define APP_KEY "xxxxxxxxxxxxxx"     // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
 #define APP_SECRET "xxxxxxxxxxxxxx"  // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
 #define TEMP_ID "xxxxxxxxxxxxxx"     // Should look like "5dc1564130xxxxxxxxxxxxxx"
